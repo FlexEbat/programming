86d44bfebfdfbcc0ad628a0c0e3d09e8d773f34d
flexebat <pulaska1337@gmail.com>
2025-04-08 23:02:04 +0700

    New changes

---
+++ С++/govnocode/char.cpp
@@ -0,0 +1,14 @@
+#include <iostream>
+#include <climits>
+
+int main ()
+{
+    using namespace std;
+    char ch;
+    cout << "Write a character: ";
+    cin >> ch;
+    cout << "Hola! ";
+    cout << "Thank you for the " << ch << " character!" << endl;
+    return 0;
+}
+
