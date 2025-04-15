43c5ab386451d40e92eccf644c5817ddc6dbd0a9
flexebat <pulaska1337@gmail.com>
2025-04-15 12:34:29 +0700

    New changes

--- C/main.c
+++ C/main.c
@@ -1,15 +1,10 @@
 #include <stdio.h>
-#include <math.h>
 
-double numberToSquare(double number) {
-	return pow(number, 3);
-}
-
-int main ()
+int main()
 {
-	double number;
-	printf("Введите число для возведения его в куб: ");
-	scanf("%lf", &number);
-	printf("Ваше число %.2lf и оно же в кубе: %.2lf\n", number, numberToSquare(number));
+	char ch;
+	for(ch = 'a'; ch <= 'z'; ch++){
+		printf("Значение ASCII для %c = %d.\n", ch, ch);
+	}
 	return 0;
 }
