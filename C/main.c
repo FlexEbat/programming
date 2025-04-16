3ff75f4962cad82fabe93da296d54ab9deadb1c0
flexebat <pulaska1337@gmail.com>
2025-04-16 22:05:42 +0700

    New changes

--- C/main.c
+++ C/main.c
@@ -2,9 +2,14 @@
 
 int main()
 {
-	char ch;
-	for(ch = 'a'; ch <= 'z'; ch++){
-		printf("Значение ASCII для %c = %d.\n", ch, ch);
+	const int secret_code = 1488;
+	int entered_code;
+
+	
+	while (entered_code != secret_code) {
+		printf("Чобi войти в рехаб, введите пiроль: ");
+		scanf("%d", &entered_code);
 	}
+	printf("Давай не заёбывай!");
 	return 0;
 }
