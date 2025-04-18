c2257d55dfd671b50edee46e3a84f058b53438a1
flexebat <pulaska1337@gmail.com>
2025-04-18 23:39:58 +0700

    New changes

--- С++/main.cpp
+++ С++/main.cpp
@@ -1,15 +1,9 @@
 #include <iostream>
-// #include <cstring>
+#include <vector>
+
 using namespace std;
-int main () 
+int main()
 {
-    const int SIZE = 20;
-    char dessert[SIZE];
-    char name[SIZE];
-    cout << "Введите ваше имя: ";
-    cin.getline(name, SIZE);
-    cout << "Введите ваш любимый десерт: ";
-    cin.getline(dessert, SIZE);
-    cout << "Привет, " << name << "! Твой любимый десерт - " << dessert << "." << endl;
+    
     return 0;
 }
