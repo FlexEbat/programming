6130d9fd0a1202a13dca6786353a8c7b60f73a69
flexebat <pulaska1337@gmail.com>
2025-04-18 14:39:00 +0700

    Add justArray program for wallet simulation and update settings

---
+++ С++/govnocode/justArray.cpp
@@ -0,0 +1,42 @@
+#include <iostream>
+using namespace std;
+ 
+int main () 
+{
+    int wallet[11];
+    cout << "Представь, что это кошелек. И здесь есть 10 ячеек. Заполни их сам." << endl;
+    cout << "Введи сколько денег лежит в первой ячейке: ";
+    cin >> wallet[0];
+    cout << "Введи сколько денег лежит во второй ячейке: ";
+    cin >> wallet[1];
+    cout << "Введи сколько денег лежит в третьей ячейке: ";
+    cin >> wallet[2];
+    cout << "Введи сколько денег лежит в четвертой ячейке: ";
+    cin >> wallet[3];
+    cout << "Введи сколько денег лежит в пятой ячейке: ";
+    cin >> wallet[4];
+    cout << "Введи сколько денег лежит в шестой ячейке: ";
+    cin >> wallet[5];
+    cout << "Введи сколько денег лежит в седьмой ячейке: ";
+    cin >> wallet[6];
+    cout << "Введи сколько денег лежит в восьмой ячейке: ";
+    cin >> wallet[7];
+    cout << "Введи сколько денег лежит в девятой ячейке: ";
+    cin >> wallet[8];
+    cout << "Введи сколько денег лежит в десятой ячейке: ";
+    cin >> wallet[9];
+    cout << "Введи сколько денег лежит в одиннадцатой ячейке: ";
+    cin >> wallet[10];
+    cout << wallet[0] << endl;
+    cout << wallet[1] << endl;
+    cout << wallet[2] << endl;
+    cout << wallet[3] << endl;
+    cout << wallet[4] << endl;
+    cout << wallet[5] << endl;
+    cout << wallet[6] << endl;
+    cout << wallet[7] << endl;
+    cout << wallet[8] << endl;
+    cout << wallet[9] << endl;
+    cout << wallet[10] << endl;
+    return 0;
+}
--- С++/main.cpp
+++ С++/main.cpp
@@ -1,7 +1,15 @@
 #include <iostream>
+// #include <cstring>
 using namespace std;
- 
 int main () 
 {
+    const int SIZE = 20;
+    char dessert[SIZE];
+    char name[SIZE];
+    cout << "Введите ваше имя: ";
+    cin.getline(name, SIZE);
+    cout << "Введите ваш любимый десерт: ";
+    cin.getline(dessert, SIZE);
+    cout << "Привет, " << name << "! Твой любимый десерт - " << dessert << "." << endl;
     return 0;
 }
