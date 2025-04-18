f087d4fda6e671e88eada72c37cbd1c3273f26d4
flexebat <pulaska1337@gmail.com>
2025-04-18 12:06:00 +0700

    Add currency conversion functions and update menu options

--- С++/main.cpp
+++ С++/main.cpp
@@ -12,7 +12,43 @@ void kgToPound()
         pound = kg * 2.20462;
         std::cout << "Вес в фунтах: " << pound << std::endl;
     }
-}    
+}   
+
+void rubToUsd() {
+    double rub;
+    const double DOLLAR = 82.03;
+    const std::string DATE = "18.04.2025";
+    std::cout << "Курс актуален на " << DATE << std::endl;
+    std::cout << "Введите сумму в рублях для перевода её в доллары: ";
+    std::cin >> rub;
+    if (rub <= 0){
+        std::cout << "Сумма не может быть <= 0" << std::endl;
+        rubToUsd();
+    } else
+    {
+        double dollar = rub / DOLLAR;
+        std::cout << "Ваши " << rub << " рублей - это " << dollar << " долларов"  << std::endl; 
+    }
+
+}
+
+void usdToRub() {
+    double rub;
+    const double DOLLAR = 82.03;
+    const std::string DATE = "18.04.2025";
+    std::cout << "Курс актуален на " << DATE << std::endl;
+    std::cout << "Введите сумму в рублях для перевода её в доллары: ";
+    std::cin >> rub;
+    if (rub <= 0){
+        std::cout << "Сумма не может быть <= 0" << std::endl;
+        rubToUsd();
+    } else
+    {
+        double rub2 = rub * DOLLAR;
+        std::cout << "Ваши " << DOLLAR << " долларов - это " << rub2 << " рублей"  << std::endl; 
+    }
+
+}
 
 int main ()
 {
@@ -20,15 +56,19 @@ int main ()
     int choice;
     do 
     {
-        cout << "Привет. Здесь ты можешь:\n 1. Из КГ в Фунты\n 2. DOLLAR TO RUB.\n";
+        cout << "Привет. Здесь ты можешь:\n 1. Из КГ в Фунты\n 2. Перевод рублей в доллары.\n 3. Перевод доллары в рубли\n";
+        cout << "!!! ВЫХОД ИЗ ПРОГРАММЫ - 0 !!!\n";
         cout << "Что ты хочешь сделать: ";
         cin >> choice;
         if (choice == 1) {
             kgToPound();
+        } else if (choice == 2) {
+            rubToUsd();
+        } else if (choice == 3) {
+            usdToRub();
         }
-        /* code */
     } while (choice != 0);
-    cout << "Выход из программы." << endl;
+    cout << "Давай не заёбывай <3" << endl;
     
     return 0;
 }
