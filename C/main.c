7389bd4974d51449b21cea31513339c5367777e0
flexebat <pulaska1337@gmail.com>
2025-04-28 22:12:06 +0700

    New changes

--- C/main.c
+++ C/main.c
@@ -1,22 +1,19 @@
 #include <stdio.h>
 
-int main()
-{
-    int code[4];
-    printf("Введите свой пароль.\n");
-    printf("Введите первый символ пароля: ");
-    scanf("%d", &code[0]);
-    printf("Введите второй символ пароля: ");
-    scanf("%d", &code[1]);
-    printf("Введите третий символ пароля: ");
-    scanf("%d", &code[2]);
-    printf("Введите четвертый символ пароля: ");
-    scanf("%d", &code[3]);
-     
-    printf("Ваш пароль: "); 
-    for(int i = 0; i < sizeof(code)/sizeof(code[0]); i++)
+#define SPACE ' '
+
+int main() {
+    char ch = getchar();
+
+    while(ch != '\n')
     {
-        printf("%d", code[i]);
+        if(ch == SPACE)
+            putchar(ch);
+        else
+           putchar(ch+1);
+        ch = getchar();
     }
+    putchar(ch);
     return 0;
 }
+
